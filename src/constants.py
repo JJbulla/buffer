c7a52d8b80fd6189982c460d8c5e11b13ec9b155
John Bulla <johnbulla@habi.co>
2024-04-19 09:30:57 -0500

    remove file

---
+++ src/constants.py
@@ -0,0 +1,5 @@
+VALID_POLICY = "Policy must be a string."
+VALID_ORDER = ["FIFO", "LIFO"]
+BUFFER_EMPTY = "Buffer is empty."
+BUFFER_NOT_FOUND = "This element is not found"
+BUFFER_EMPTY_ERROR = "BufferEmptyError"
